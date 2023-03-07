49c59320461eb53ae250253a28da2c3af593e723
mqtech25 <mqtech25@gmail.com>
2023-03-07 16:46:08 +0500

    some changes

--- customize/js/custom.js
+++ customize/js/custom.js
@@ -1,6 +1,28 @@
-const featurelinks=document.querySelectorAll(".feature_link");
-featurelinks.addEventListener("click", (event)=>{
-   event.preventDefault();
-    const href= this.href
-    console.log(href);
-});
+// const nextStepBtn = document.getElementById("nextStepFeature");
+// const tabs = document.querySelectorAll(".stepsTab");
+// nextActive = "";
+// currentItem = "";
+// nextStepBtn.addEventListener("click", (event) => {
+//   tabs.forEach((item, i) => {
+//     if (item.classList.contains("active") && i < tabs.length) {
+//       //   console.log(item, i);
+//       currentItem = item;
+//       //   console.log(item.nextElementSibling);
+//     }
+//   });
+//   currentItem.classList.remove("active");
+//   currentItem.classList.remove("show");
+//   if (currentItem.allNextElementSibling.classList.contains("stepsTab")) {
+//     currentItem.classList.add("active", "show");
+//   }
+//   //   nextItem.classList.add("active", "show");
+//   //   currentItem.nextElementSibling.classList
+//   //     .contains("stepsTab")
+//   //     .add("active", "show");
+
+//   window.scrollTo({
+//     top: currentItem.offsetTop,
+//     left: 0,
+//     behavior: "smooth",
+//   });
+// });
